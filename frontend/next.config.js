b8977197136ffb69c0b226a0ee83659f5bf90631
pickyspoons <none@none.com>
2021-12-10 14:43:56 -0500

    Removing base path and redeploying front end

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,5 @@
 const isProd = (process.env.NODE_ENV || 'production') === 'production'
 
 module.exports = {
-  reactStrictMode: true,
-  basePath: isProd ? '/concave_x_colors_nft' : '',
-  assetPrefix: isProd ? '/concave_x_colors_nft/' : ''
+  reactStrictMode: true
 }
