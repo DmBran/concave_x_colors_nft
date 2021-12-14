8a1ea318d51b5ef834b5142180d539273e8390bf
PC <jan@heyjan.de>
2021-12-14 17:44:22 +0100

    Hero revamp, added first Gallery frame, added Team Section, changed Font to Lato

--- frontend/components/head.js
+++ frontend/components/head.js
@@ -1,13 +1,15 @@
 import Head from 'next/head'
 
+
 export const MetaHead = () => {
-  return (
-    <Head>
-      <meta charset="utf-8" />
-      <meta name="color-scheme" content="dark light" />
-      <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <title>Sync ∞ Colors</title>
-    </Head>
-  )
+    return (
+        <Head>
+            <meta charset="utf-8" />
+            <meta name="color-scheme" content="dark light" />
+            <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <title>Sync ∞ Colors</title>
+            <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet" />
+        </Head>
+    )
 }
--- frontend/components/navbar.js
+++ frontend/components/navbar.js
@@ -20,94 +20,96 @@ export const Navbar = (props) => {
   }
 
   return (
-    <div>
-      <nav className={styles.nav}>
-        <div className="container mx-auto flex flex-wrap p-4 md:mb-4">
-          <Link href="/">
-            <p className="flex title-font font-medium items-center text-gray-900 md:mb-0">
-              <span className="mr-2 text-xl">SYNC</span>
-              <span
-                style={{ fontSize: '40px', top: '-2px' }}
-                className="relative text-xl"
-              >
+      <div>
+        <nav className="bg-gray-300 bg-opacity-50">
+          <div className="container mx-auto flex flex-wrap p-4 md:mb-4">
+            <Link href="/">
+              <p className="flex title-font font-bold items-center text-gray-900 md:mb-0">
+                <span className="mr-2 text-xl">SYNC</span>
+                <span
+                    style={{ fontSize: '40px', top: '-2px' }}
+                    className="relative text-xl"
+                >
                 {' '}
-                &infin;
+                  &infin;
               </span>
-              <span className="ml-2 text-xl">COLORS</span>
-            </p>
-          </Link>
-          <button
-            className="hamburger align-right inline-flex p-3 hover:text-indigo-500 rounded lg:hidden ml-auto outline-none"
-            onClick={handleClick}
-          >
-            <svg
-              className="w-6 h-6"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
+                <span className="ml-2 text-xl">COLORS</span>
+              </p>
+            </Link>
+
+            {/*Mobile Button goes here*/}
+            <button
+                className="hamburger align-right inline-flex p-3 hover:text-indigo-500 rounded lg:hidden ml-auto outline-none"
+                onClick={handleClick}
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
-          </button>
-          {/*Note that in this div we will use a ternary operator to decide whether or not to display the content of the div  */}
-          <div
-            className={`${
-              active ? '' : 'hidden'
-            }   w-full lg:inline-flex lg:flex-grow lg:w-auto`}
-          >
-            <div className="lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto">
-              <Link href="/">
-                <a className="text-black uppercase lg:inline-flex lg:w-auto w-full px-3 py-2 rounded font-bold items-center justify-center hover:text-indigo-500">
-                  Home
+              <svg
+                  className="w-6 h-6"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            </button>
+            {/*Note that in this div we will use a ternary operator to decide whether or not to display the content of the div  */}
+            <div
+                className={`${
+                    active ? '' : 'hidden'
+                }   w-full lg:inline-flex lg:flex-grow lg:w-auto`}
+            >
+              <div className="lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto">
+                <Link href="/">
+                  <a className="text-black uppercase lg:inline-flex lg:w-auto w-full px-3 py-2 rounded font-bold items-center justify-center hover:text-indigo-500">
+                    Home
+                  </a>
+                </Link>
+                {1 && (
+                    <Link href="/mint">
+                      <a className="text-black uppercase lg:inline-flex lg:w-auto w-full px-3 py-2 rounded font-bold items-center justify-center hover:text-indigo-500">
+                  Mint
                 </a>
               </Link>
-              {1 && (
-                <Link href="/mint">
-                  <a className="text-black uppercase lg:inline-flex lg:w-auto w-full px-3 py-2 rounded font-bold items-center justify-center hover:text-indigo-500">
-                    Mint
-                  </a>
-                </Link>
-              )}
-              {1 && (
-                <Link href="/nfts">
-                  <a className="text-black uppercase lg:inline-flex lg:w-auto w-full px-3 py-2 rounded font-bold items-center justify-center hover:text-indigo-500">
-                    My NFTs
-                  </a>
-                </Link>
-              )}
-              {context.active && context.account && (
-                <a
-                  target="_blank"
-                  href={`https://etherscan.io/address/${context.account}`}
-                >
-                  <p className="text-black uppercase lg:inline-flex lg:w-auto w-full px-3 py-2 rounded font-bold items-center justify-center hover:text-indigo-500">
-                    {context.account.substring(0, 5) +
-                      '....' +
-                      context.account.substring(11, 16)}
-                  </p>
+                )}
+                {1 && (
+                    <Link href="/nfts">
+                      <a className="text-black uppercase lg:inline-flex lg:w-auto w-full px-3 py-2 rounded font-bold items-center justify-center hover:text-indigo-500">
+                  My NFTs
                 </a>
-              )}
-              {!context.active && (
-                <button
-                  type="button"
-                  className={
-                    'uppercase inline-flex ring-1 ring-indigo-500 items-center  py-1 px-3 focus:outline-none rounded text-base'
-                  }
-                  onClick={activateMM}
-                >
+                    </Link>
+                )}
+                {context.active && context.account && (
+                    <a
+                        target="_blank"
+                        href={`https://etherscan.io/address/${context.account}`}
+                    >
+                      <p className="text-black uppercase lg:inline-flex lg:w-auto w-full px-3 py-2 rounded font-bold items-center justify-center hover:text-indigo-500">
+                        {context.account.substring(0, 5) +
+                        '....' +
+                        context.account.substring(11, 16)}
+                      </p>
+                    </a>
+                )}
+                {!context.active && (
+                    <button
+                        type="button"
+                        className={
+                          'uppercase inline-flex ring-1 ring-indigo-500 items-center  py-1 px-3 focus:outline-none rounded text-base'
+                        }
+                        onClick={activateMM}
+                    >
                   <p className={'font-bold'}>Connect Wallet</p>
-                </button>
-              )}
+                    </button>
+                )}
+            </div>
             </div>
           </div>
-        </div>
-      </nav>
-    </div>
+        </nav>
+      </div>
   )
 }
