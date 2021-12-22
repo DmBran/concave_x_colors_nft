52e93641d70ab3fa84970d987a21ccaab9b671f2
concave-dash <kwerkee12@gmail.com>
2021-12-22 09:03:12 -0500

    updating color contract address

--- packages/hardhat/contracts/SyncXColors.sol
+++ packages/hardhat/contracts/SyncXColors.sol
@@ -25,7 +25,7 @@ contract SyncXColors is ERC721Enumerable, Ownable {
 
   // Declare Public
   address public constant THE_COLORS =
-    address(0x3C4CfA9540c7aeacBbB81532Eb99D5E870105CA9);
+    address(0x9fdb31F8CE3cB8400C7cCb2299492F2A498330a4);
   //address public THE_COLORS =
     //address(0x3C4CfA9540c7aeacBbB81532Eb99D5E870105CA9);
   uint256 public constant mintPrice = 0.05 ether; // Price per mint
