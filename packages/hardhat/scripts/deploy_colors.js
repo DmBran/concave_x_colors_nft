7357413f3233510ad41e26dc2e510f69367c1bd7
concave-dash <kwerkee12@gmail.com>
2021-12-21 21:41:16 -0500

    adding deploy colors script

---
+++ packages/hardhat/scripts/deploy_colors.js
@@ -0,0 +1,14 @@
+async function main() {
+  const SyncXColors = await ethers.getContractFactory('TheColors')
+
+  // Start deployment, returning a promise that resolves to a contract object
+  const mySyncXColors = await SyncXColors.deploy()
+  console.log('Contract deployed to address:', mySyncXColors.address)
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
